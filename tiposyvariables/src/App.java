7453405edc223a2a8a724afbcfa466021ec7965d
Oscar Silvera Cruz <ojsilvera@gmail.com>
2023-06-14 12:26:44 -0500

    asignando tipo de variable y reasignacion de su valor

--- tiposyvariables/src/App.java
+++ tiposyvariables/src/App.java
@@ -1,5 +1,16 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        // declaramos el tipo de variable y le asignamos el valor que necesitamos
+        int edad = 40;
+        // imprimimos la variable anteriormente creada llamandola directamente
+        // como argumento del metodo println de java para imprimir en pantalla
+        System.out.println(edad);
+        // como el tipo de dato de edad ya fue declarado antes solo le reasignamos el
+        // valor
+        // y reimprimimos su valor
+        edad = 55;
+        System.out.println(edad);
     }
 }
