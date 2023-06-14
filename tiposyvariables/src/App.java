d99fcfb3e29ebb52f0f230117393457b2500b224
Oscar Silvera Cruz <ojsilvera@gmail.com>
2023-06-14 13:39:07 -0500

    Tipos de numeros segun su peso

--- tiposyvariables/src/App.java
+++ tiposyvariables/src/App.java
@@ -16,5 +16,35 @@ public class App {
         // podemos asignar valores aritmeticos
         edad = 19 + 21;
         System.out.println(edad);
+
+        // concatenamos con el signo +
+        System.out.println("mi edad es: " + edad + " Anos");
+
+        // double para decimales y es posible asignar un valor entero al double
+        // pero estariamos asignando mas espacio de memoria que el requerido
+        // el double es usado en operaciones que arrojen decimales como resultado
+        // si ejecutamos una operacion que de reultado decima, java redondea y muestra
+        // solo un entero
+
+        double peso = 110.5;
+        double pesoObjetivo = 110.5 - 20;
+
+        // los imprimo en la consola
+        System.out.println("mi peso actual es: " + peso + "Kg");
+        System.out.println("mi peso objetivo es: " + pesoObjetivo + "Kg");
+
+        // coversion de datos para compatibilizarlos entre ellos
+        // coloco (que tipo de datos quiero forzar) la variable que deseo forzar
+        // se le conoce como cast en java
+        // solo tomara la parte entera pero seraun entero no un decimal
+
+        int pesoEntero = (int) pesoObjetivo;
+        System.out.println(pesoEntero);
+
+        // para numeros mayores a 2^31 tantopositivos como negativos se usa
+        // el tipo de variable long 122222222L
+        // para numeros pequeños short o byte
+        // decimales pequeños float 0.25F
+
     }
 }
