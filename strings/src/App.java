821b7409dc674479da3e281668a4f27e351b5aed
Oscar Silvera Cruz <ojsilvera@gmail.com>
2023-06-15 10:59:18 -0500

    manejo de strings

---
+++ strings/src/App.java
@@ -0,0 +1,28 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // probando el trabajo con cadenas de texto
+        // char solo almacena un solo caracter o numero(hace ref a la la codificacion
+        // ascii) y no puede estar vacio.
+        // se utilizan '' comillas simples
+
+        char letra = 'a';
+        System.out.println(letra);
+
+        // el resultado corresponde a un caracter de la trabla ascii
+        letra = 65 + 1;
+        System.out.println(letra);
+
+        // al realizar una operacion entre un char y un entero, el entero debe
+        // convertirse a char
+        // ya que por defecto java convierte toda la operasion al tipo de dato mas
+        // grande
+        char letra2 = (char) (letra + 1);
+        System.out.println(letra2);
+
+        // para frases completas
+
+        String palabra = "En busqueda de trabajo xd";
+        System.out.println(palabra + letra2);
+
+    }
+}
