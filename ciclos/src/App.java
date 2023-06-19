3d21917c26405b1b31356dbcd292adb67f1869c8
Oscar Silvera Cruz <ojsilvera@gmail.com>
2023-06-19 14:14:47 -0500

    Imprimir numeros del 1 al 10

--- ciclos/src/App.java
+++ ciclos/src/App.java
@@ -1,5 +1,14 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // Los ciclos en java ejemplo imprimir los numeros del 1 - 10
+        int numero = 1; // inicializamos la variable
+        while (numero <= 10) {
+
+            System.out.println(numero);
+            numero = numero + 1; // incrementamos el valo de la valiable para alcanzar le tope de la
+                                 // condicion
+
+        }
+
     }
 }
