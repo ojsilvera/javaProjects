81a6c19ec6bb3226e6ecec5a360dbdb55176fa93
Oscar Silvera Cruz <ojsilvera@gmail.com>
2023-06-19 14:33:51 -0500

    mas formas de expresar incremento e inicializo el contador a cero

--- ciclos/src/App.java
+++ ciclos/src/App.java
@@ -1,13 +1,14 @@
 public class App {
     public static void main(String[] args) throws Exception {
         // Los ciclos en java ejemplo imprimir los numeros del 1 - 10
-        int numero = 1; // inicializamos la variable
+        int numero = 0; // inicializamos la variable
         while (numero <= 10) {
 
             System.out.println(numero);
-            numero = numero + 1; // incrementamos el valo de la valiable para alcanzar le tope de la
-                                 // condicion
-
+            // incrementamos el valo de la valiable para alcanzar le tope de la condicion
+            numero = numero + 1;
+            // numero++; -> otra forma de expresar el incremento en 1 unidad del contador
+            // numero += #de incremento, numero += 1, otra manera de expresar incremento
         }
 
     }
