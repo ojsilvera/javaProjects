dc7ef2ecaadab03a6db2a03451b6ffa2dbf7a6df
Oscar Silvera Cruz <ojsilvera@gmail.com>
2023-06-19 15:34:05 -0500

    alcance de las variables

--- ciclos/src/App.java
+++ ciclos/src/App.java
@@ -11,5 +11,34 @@ public class App {
             // numero += #de incremento, numero += 1, otra manera de expresar incremento
         }
 
+        // hallar la suma de los numeros del 1 - 10 mostrando todas las sumas
+        int contador = 0; // inicializamos la variable contador
+        int suma = 0; // inicializamos la variable suma
+
+        while (contador <= 10) {
+
+            suma = suma + contador;
+            System.out.println("paso: " + contador + " " + "suma: " + suma);
+            // incrementamos el valo de la valiable para alcanzar le tope de la condicion.
+            // suma lod dos que son el resultado anterior y el nuevo paso.
+            contador++;
+            // incrementa el paso para moverse por los numero, en incremento de +1.
+
+        }
+
+        // hallar la suma de los numeros del 1 - 10 mostrando todas las sumas
+        int paso = 0; // inicializamos la variable paso
+        int total = 0; // inicializamos la variable suma
+
+        while (paso <= 10) {
+
+            total = total + paso; // suma el numero anterior con el actual
+            paso++;
+            // incrementa el paso para moverse por los numero, en incremento de +1.
+        }
+
+        System.out.println("suma Total: " + total);
+        // incrementamos el valo de la valiable para alcanzar le tope de la condicion.
+        // suma los dos que son el resultado anterior y el nuevo paso.
     }
 }
