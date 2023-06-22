1f5aa9cf6459d8509dd764da68a2ad76de4366eb
Oscar Silvera Cruz <ojsilvera@gmail.com>
2023-06-22 14:33:51 -0500

    adicionando segunda cuenta

--- POO/src/CrearCuenta.java
+++ POO/src/CrearCuenta.java
@@ -4,7 +4,28 @@ public class CrearCuenta {
         // crear cuenta
         // para llamar al objeto cuenta, primero debemos instanciarla
         // TiposdeObjeto(nombre de la clase a usar) nombreObjeto = new objeto(param);
+
         Cuenta primeraCuenta = new Cuenta();
+
+        // llamamos al objeto con su atributo para almacenar un nuevo valor
+
+        primeraCuenta.saldo = 1000;
+        primeraCuenta.agencia = 25;
+        primeraCuenta.numero = 14376817;
+        primeraCuenta.titular = "Oscar Silvera";
+
+        // para tener una segunda cuenta creamos un nuevo espacio en memoria con el
+        // comando new
+
+        Cuenta segundaCuenta = new Cuenta();
+
+        segundaCuenta.saldo = 10000;
+        segundaCuenta.agencia = 35;
+        segundaCuenta.numero = 25386877;
+        segundaCuenta.titular = "Mariluz Cardona";
+
+        System.out.println(primeraCuenta.titular);
+        System.out.println(segundaCuenta.titular);
 
     }
 }
