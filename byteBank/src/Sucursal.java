087492e6b3571141ca04a139788d9c0e77a8c261
Oscar Silvera Cruz <ojsilvera@gmail.com>
2023-06-27 15:26:34 -0500

    se agregaron los atributos, consultas y el metodo crear a la entidad sucursal

--- byteBank/src/Sucursal.java
+++ byteBank/src/Sucursal.java
@@ -1,3 +1,37 @@
 public class Sucursal {
+    // campos
+    private int idSucursal;
+    private String direccion;
+    private String telefono;
 
+    // consultar
+    public int getIdSucursal() {
+        return idSucursal;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    // acciones
+    // crear
+    public boolean setCreatedSucursal(int codSucursal, String direccionSucursal, String telefonoSucursal) {
+
+        if (codSucursal != 0 && telefonoSucursal != "") {
+
+            this.idSucursal = codSucursal;
+            this.direccion = direccionSucursal;
+            this.telefono = telefonoSucursal;
+            return true;
+
+        } else {
+
+            return true;
+
+        }
+    }
 }
