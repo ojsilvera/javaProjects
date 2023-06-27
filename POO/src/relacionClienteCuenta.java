219e959f2f321f30ca7574d1ea59b790d86324f1
Oscar Silvera Cruz <ojsilvera@gmail.com>
2023-06-27 10:16:30 -0500

    independizando validacione,separando logica de negocio y agregando mensajes para el cliente

--- POO/src/relacionClienteCuenta.java
+++ POO/src/relacionClienteCuenta.java
@@ -9,28 +9,41 @@ public class relacionClienteCuenta {
 
         // creando cliente
         boolean clienteValido = defaultCliente.setCliente("Oscar Slivera", "3165656110", "6212456897");
+
+        // validando creacion del cliente
         if (clienteValido) {
-            // asignando datos a la cuenta
-            boolean opercionValida = cuentaCliente.setCuenta(10000, defaultCliente, 5, 14376817);
-
-            if (opercionValida) {
-                // obtenemos datos del objeto cliente a traves del capo referenciado en el
-                // objeto cuenta
-                System.out.println("Nombre del cliente: " + cuentaCliente.getTitular().getNombre());
-                System.out.println("Documento del cliente: " + cuentaCliente.getTitular().getDocumento());
-                System.out.println("Saldo de la cuenta: " + cuentaCliente.getSaldo());
-
-                // deposito a cuenta propia
-                // cuentaCliente.depositar(10000);
-
-                // System.out.println("Saldo cuenta post deposito: " +
-                // System.out.println("buevo saldo: " + cuentaCliente.getSaldo());
-            } else {
-                System.out.println("Paametros de cuenta incompletos");
-            }
+            System.out.println("Cliente creado satisfactoriamente: ");
         } else {
             System.out.println("Parametros de Cliente incompletos");
+
         }
+
+        // asignando datos a la cuenta
+        if (clienteValido) {
+            cuentaCliente.setCuenta(10000, defaultCliente, 1, 14376817);
+            System.out.println("Cuenta creada satisfactoriamente");
+        } else {
+            System.out.println("No se reporta cliente para esta operacion");
+
+        }
+
+        // validando creacion de la cuenta
+        if (cuentaCliente.getNumero() != 0) {
+            // obtenemos datos del objeto cliente a traves del capo referenciado en el
+            // objeto cuenta
+            System.out.println("Nombre del cliente: " + cuentaCliente.getTitular().getNombre());
+            System.out.println("Documento del cliente: " + cuentaCliente.getTitular().getDocumento());
+            System.out.println("Saldo de la cuenta: " + cuentaCliente.getSaldo());
+
+        } else {
+            System.out.println("Paametros de cuenta incompletos");
+        }
+
+        // deposito a cuenta propia
+        // cuentaCliente.depositar(10000);
+
+        // System.out.println("Saldo cuenta post deposito: " +
+        // System.out.println("buevo saldo: " + cuentaCliente.getSaldo());
 
     }
 
