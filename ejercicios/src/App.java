d4570bd6d874315fea40a59c97634fdfea96c14f
Oscar Silvera Cruz <ojsilvera@gmail.com>
2023-07-14 12:45:09 -0500

    implementar saludo con una clase

---
+++ ejercicios/src/App.java
@@ -0,0 +1,11 @@
+import uno_saludo.Saludo;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        Saludo hola = new Saludo();
+
+        System.out.println(hola.Saludar());
+
+    }
+}
---
+++ ejercicios/src/uno_saludo/Saludo.java
@@ -0,0 +1,7 @@
+package uno_saludo;
+
+public class Saludo {
+    public String Saludar() {
+        return "Hola mundo";
+    }
+}
