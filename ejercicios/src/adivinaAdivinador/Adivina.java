b7d77eeca9fa59435ad94ae74b26ae649dc38c4a
Oscar Silvera Cruz <ojsilvera@gmail.com>
2023-07-15 23:02:53 -0500

    Bug con el cierre del llamado a scanner, reemplazar por try cath el .close()

--- ejercicios/src/adivinaAdivinador/Adivina.java
+++ ejercicios/src/adivinaAdivinador/Adivina.java
@@ -5,49 +5,69 @@ import java.util.Scanner;
 public class Adivina {
 
     private double pInferior;
+    private double pSuperior;
 
-    public void adivinar(int miNumero) {
+    // Math.floor(Math.random() * (MAX - MIN + 1)) + MIN; genera aleatorio entre 0 y
+    // 100
+    static double numeroInterno = Math.floor(Math.random() * (100 - 0 + 1)) + 0;
 
-        // Math.floor(Math.random() * (MAX - MIN + 1)) + MIN; genera aleatorio entre 0 y
-        // 100
-        double numeroInterno = Math.floor(Math.random() * (100 - 0 + 1)) + 0;
+    public void adivinar(double miNumero) {
 
         // evalua si es el numero
         while (miNumero != numeroInterno) {
-            entregaPista(numeroInterno);
+
+            entregaPista(miNumero);
+
         }
 
-        solicitar();
+        meHasPillado(miNumero);
 
     }
 
     public void solicitar() {
 
         // primer pedido de numero
-        Scanner dato = new Scanner(System.in);
-        System.out.println("Digite numero: ");
-        int miNumero = dato.nextInt();
-        dato.close();
+        try (Scanner dato = new Scanner(System.in);) {
+            System.out.println("Digite numero: ");
+            double miNumero = dato.nextDouble();
+            // llamamos a adivinar
+            adivinar(miNumero);
+        } catch (Exception e) {
 
-        // lamamos a adivinar
-        adivinar(miNumero);
+            e.getCause();
+        }
 
     }
 
-    public void entregaPista(double numeroInterno) {
+    public void entregaPista(double miNumero) {
 
-        if (numeroInterno - 50 < 0) {
+        if (miNumero - 50 < 0) {
 
             this.pInferior = 0;
 
         } else {
 
-            this.pInferior = numeroInterno - 50;
+            this.pInferior = miNumero - 50;
         }
 
-        System.out.println("El numero esta entre: " + pInferior);
+        if (miNumero + 50 > 100) {
+
+            this.pSuperior = 100;
+
+        } else {
+
+            this.pSuperior = miNumero + 50;
+        }
+
+        System.out.println("El numero esta entre: " + pInferior + " y " + pSuperior);
 
         solicitar();
+
+    }
+
+    public void meHasPillado(double miNumero) {
+
+        System.out.println("el numero secreto es: " + miNumero);
 
     }
 
